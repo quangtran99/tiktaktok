227c800286adacd7ef7fe6e7cced2564fa2f5e0b
Quang Tran <tranvietquang3077@gmail.com>
2020-07-29 10:57:54 +0700

    sleepless

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Board from "./components/Board" 
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+
+
+    this.state={
+      username:"Quang",
+      nextPlayer: true, //true: X False : )
+      squareList: ['','','','','','','','',''],
+      // array: Array(9).fill(null),
+      winner: "",
+      gameOver: false,
+    }
+  }
+
+  setParentsState=(obj)=>{
+    this.setState(obj)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Tic Tac Toe</h1>
+    <h3>User name :{this.state.username}</h3>
+  
+    <Board squareList={this.state.squareList} 
+           setParentsState={this.setParentsState}
+           nextPlayer={this.state.nextPlayer}
+           winner={this.state.winner}
+           gameOver={this.state.gameOver} />
+    <ol> History
+      
+    </ol>
+    <ol>Ranking</ol>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import Square from './Square'
+
+export default class Board extends Component {
+
+    calculateWinner = (squares) => {
+
+        console.log('array', squares)
+
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+
+            const [a, b, c] = lines[i];
+
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+
+                if (squares[a] === 'X') {
+                    return alert("x win");
+                } else {
+                    return alert("O win")
+                }
+
+
+            }
+        }
+        return null;
+    }
+
+    selectSquare=(id)=>{
+        let array = this.props.squareList
+        let gameOverValue = this.props.gameOver
+
+        if (array[id] !== '') {
+            return
+        }
+
+        array[id]=this.props.nextPlayer ? "X" : "O"
+
+
+
+        let winnerValue = this.calculateWinner (array);
+
+        
+
+        if(winnerValue == null && !array.includes('')) {
+            gameOverValue = !gameOverValue;
+        } 
+
+        this.props.setParentsState({
+            squareList: array ,
+            nextPlayer: !this.props.nextPlayer , 
+            winner: winnerValue,
+            gameOver: gameOverValue,
+        })
+    }
+
+
+
+    // 1. calculate winner , if X wins show X wins
+    // 2. if no winner, game over
+    // 4. show history ranking
+    render() {
+        return (
+            <div>
+                 <h3>nextPlayer: {this.props.nextPlayer? 'X' : 'O'}</h3>
+                 <h3>{this.props.gameOver == true ? `gameOver` : `winner is: ${this.props.winner}` }</h3>
+                <div style={{ display: "flex" }}>
+                    <Square id={0} selectSquare={this.selectSquare} value={this.props.squareList[0]}/>
+                    <Square id={1} selectSquare={this.selectSquare} value={this.props.squareList[1]}/>
+                    <Square id={2} selectSquare={this.selectSquare} value={this.props.squareList[2]}/>
+                </div>
+                <div style={{ display: "flex" }}>
+                    <Square id={3} selectSquare={this.selectSquare} value={this.props.squareList[3]}/>
+                    <Square id={4} selectSquare={this.selectSquare} value={this.props.squareList[4]}/>
+                    <Square id={5} selectSquare={this.selectSquare} value={this.props.squareList[5]}/>
+                </div>
+                <div style={{ display: "flex" }}>
+                    <Square id={6} selectSquare={this.selectSquare} value={this.props.squareList[6]}/>
+                    <Square id={7} selectSquare={this.selectSquare} value={this.props.squareList[7]}/>
+                    <Square id={8} selectSquare={this.selectSquare} value={this.props.squareList[8]}/>
+                </div>
+            </div>
+        )
+    }
+}
